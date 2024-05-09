7f1ff204b46f88c9c1a40b446e6f6c7b81389f2d
Danzille <chopper9751@mail.ru>
2024-05-09 23:18:29 +0500

    End Task2

---
+++ ET_t2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int AckermannFunction(int m, int n) 
+    { 
+        if (m == 0) 
+            return n + 1; 
+        else if (n == 0) 
+            return AckermannFunction(m - 1, 1); 
+        else 
+            return AckermannFunction(m - 1, AckermannFunction(m, n - 1)); 
+    } 
+System.Console.WriteLine(AckermannFunction(3,5));
